9dcc3ea66fa18255f57d133826649d2205c79528
MATHEUS CARVALHO <matheuscarvalho.pe@hotmail.com>
2023-01-06 14:28:02 -0300

    visto tipos logicos, literais, inteiros e reais.

--- aprendendoSintaxe/src/edu/estudos/tiposDeDados/TiposDeDados.java
+++ aprendendoSintaxe/src/edu/estudos/tiposDeDados/TiposDeDados.java
@@ -1,5 +1,21 @@
 package edu.estudos.tiposDeDados;
 
 public class TiposDeDados {
-    
+
+    boolean verifcado = true; // false;
+    char sexo = 'M'; //'F';
+    byte idade = 123;
+    short ano = 2023;
+    int cep = 53020256;// se começar com zero deve ser passado para String
+    long cpf = 49523501846L;// deve conter o L no final do valor da variavel para poder "confirmar" que a
+                            // mesma é do tipo long
+    float pi = 3.14F;// deve conter o F no dinal para poder "confirmar que a mesma é do tipo float"
+    double salario = 1275.33;
+
+    //constante
+    //palavra final reservada para declaração de constantes
+    final int CONSTANTE = 18;
+      
 }
+
+
